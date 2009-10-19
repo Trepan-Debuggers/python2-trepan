a4fee4a18ae804981aabc35bec7ec73f2994465c
rocky.bernstein <rocky.bernstein@23f25e0e-064c-11de-b8a2-c1cd390dc4a2>
2009-10-19 00:00:28 +0000

    info file xx lines -> info file xx brkpts
    git-svn-id: https://pydbgr.googlecode.com/svn/trunk@149 23f25e0e-064c-11de-b8a2-c1cd390dc4a2

--- pydbgr/processor/command/info_subcmd/file.py
+++ pydbgr/processor/command/info_subcmd/file.py
@@ -27,13 +27,13 @@ Show information about the current file. If no filename is given and
 the program is running then the current file associated with the
 current stack entry is used. Sub options which can be shown about a file are:
 
-lines -- Line numbers where there are statement boundaries. 
-         These lines can be used in breakpoint commands.
-sha1  -- A SHA1 hash of the source text. This may be useful in comparing
-         source code.
-size  -- The number of lines in the file.
+brkpts -- Line numbers where there are statement boundaries. 
+          These lines can be used in breakpoint commands.
+sha1   -- A SHA1 hash of the source text. This may be useful in comparing
+          source code.
+size   -- The number of lines in the file.
 
-all   -- All of the above information.
+all    -- All of the above information.
 '''
 
     min_abbrev = 2
@@ -90,7 +90,7 @@ all   -- All of the above information.
                 self.msg("SHA1 is %s." % pyficache.sha1(canonic_name))
                 processed_arg = True
                 pass
-            if arg in ['all', 'lines']:
+            if arg in ['all', 'brkpts']:
                 lines = pyficache.trace_line_numbers(canonic_name)
                 self.msg("Possible breakpoint line numbers:")
                 fmt_lines = columnize.columnize(lines, ljust = False,

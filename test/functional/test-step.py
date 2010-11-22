ac60ddcd7e3116a40419b301cf5e2525439f3d87
rocky.bernstein <rocky.bernstein@23f25e0e-064c-11de-b8a2-c1cd390dc4a2>
2010-11-22 19:36:05 +0000

    Start to reinstate tests.
    git-svn-id: https://pydbgr.googlecode.com/svn/trunk@242 23f25e0e-064c-11de-b8a2-c1cd390dc4a2

--- test/functional/test-step.py
+++ test/functional/test-step.py
@@ -6,7 +6,7 @@ from fn_helper import strarray_setup, compare_output
 class TestStep(unittest.TestCase):
     print "test ", __file__, "skipped"
 
-    def NO__test_step_same_level(self):
+    def test_step_same_level(self):
         return
 
         # See that we can step with parameter which is the same as 'step 1'
@@ -19,9 +19,12 @@ class TestStep(unittest.TestCase):
         ##############################
         d.core.stop()
         out = ['-- x = 5',
-               '-- y = 6']
+               '-- y = 6\n']
         compare_output(self, out, d, cmds)
+        return
 
+    def NO__test_step_computed_valued(self):
+        return
         # See that we can step with a computed count value
         cmds = ['step 5-3', 'continue']
         d = strarray_setup(cmds)
@@ -33,7 +36,7 @@ class TestStep(unittest.TestCase):
         ##############################
         d.core.stop(options={'remove': True})
         out = ['-- x = 5',
-               '-- z = 7']
+               '-- z = 7\n']
         compare_output(self, out, d, cmds)
 
         # Test step>
@@ -49,25 +52,25 @@ class TestStep(unittest.TestCase):
         ##############################
         d.core.stop(options={'remove': True})
         out = ['-- x = 5',
-               '-> def foo():']
+               '-> def foo():\n']
         compare_output(self, out, d, cmds)
 
-        # Test step!
-        cmds = ['step!', 'continue']
-        d = strarray_setup(cmds)
-        d.core.start()
-        ##############################
-        x = 5
-        try:
-            y = 2
-            z = 1/0
-        except:
-            pass
-        ##############################
-        d.core.stop(options={'remove': True})
-        out = ['-- x = 5',
-               '!! z = 1/0']
-        compare_output(self, out, d, cmds)
+        # # Test step!
+        # cmds = ['step!', 'continue']
+        # d = strarray_setup(cmds)
+        # d.core.start()
+        # ##############################
+        # x = 5
+        # try:
+        #     y = 2
+        #     z = 1/0
+        # except:
+        #     pass
+        # ##############################
+        # d.core.stop(options={'remove': True})
+        # out = ['-- x = 5',
+        #        '!! z = 1/0\n']
+        # compare_output(self, out, d, cmds)
 
         # Test "step" with sets of events. Part 1
         cmds = ['step call exception',
@@ -85,35 +88,35 @@ class TestStep(unittest.TestCase):
         except:
             pass
         z = 1
-        ##############################
-        d.core.stop(options={'remove': True})
-        out = ['-- x = 5',
-               '-> def foo1():',
-               '!! raise Exception']
-        compare_output(self, out, d, cmds)
+        # ##############################
+        # d.core.stop(options={'remove': True})
+        # out = ['-- x = 5',
+        #        '-> def foo1():',
+        #        '!! raise Exception']
+        # compare_output(self, out, d, cmds)
 
-        # Test "step" will sets of events. Part 2
-        cmds = ['step call exception 1+0',
-                'step call exception 1', 'continue']
-        d = strarray_setup(cmds)
-        d.core.start()
-        ##############################
-        x = 5
-        try:
-            def foo2():
-                y = 2
-                raise Exception
-                return
-            foo2()
-        except:
-            pass
-        z = 1
-        ##############################
-        d.core.stop(options={'remove': True})
-        out = ['-- x = 5',
-               '-> def foo2():',
-               '!! raise Exception']
-        compare_output(self, out, d, cmds)
+        # # Test "step" will sets of events. Part 2
+        # cmds = ['step call exception 1+0',
+        #         'step call exception 1', 'continue']
+        # d = strarray_setup(cmds)
+        # d.core.start()
+        # ##############################
+        # x = 5
+        # try:
+        #     def foo2():
+        #         y = 2
+        #         raise Exception
+        #         return
+        #     foo2()
+        # except:
+        #     pass
+        # z = 1
+        # ##############################
+        # d.core.stop(options={'remove': True})
+        # out = ['-- x = 5',
+        #        '-> def foo2():',
+        #        '!! raise Exception']
+        # compare_output(self, out, d, cmds)
 
         return
 

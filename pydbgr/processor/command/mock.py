8a42d7e10d1dcaf015522b6583180d2693b7e64c
rocky <rocky@gnu.org>
2013-03-11 05:24:28 -0400

    3k compatibility

--- pydbgr/processor/command/mock.py
+++ pydbgr/processor/command/mock.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
 #   Copyright (C) 2009-2010, 2013 Rocky Bernstein
 #
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-#    02110-1301 USA.
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ''' Not a command. A stub class used by a command in its 'main' for
 demonstrating how the command works.'''
 
 import os, sys
 from import_relative import import_relative
-import_relative('lib', '...', 'pydbgr')
 breakpoint = import_relative('breakpoint', '...lib', 'pydbgr')
 default    = import_relative('default', '...lib', 'pydbgr') # Default settings
 

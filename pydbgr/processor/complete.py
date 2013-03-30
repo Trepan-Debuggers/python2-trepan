e3c7b08a0fe6dba8c280e4ac8bd2b59c96e84f54
rocky <rocky@gnu.org>
2013-03-30 10:54:25 -0400

    More tests

--- pydbgr/processor/complete.py
+++ pydbgr/processor/complete.py
@@ -1,3 +1,4 @@
+# Copyright (C) 2013 Rocky Bernstein <rocky@gnu.org>
 # Completion part of CommandProcessor
 import re
 
@@ -5,41 +6,78 @@ def complete_token(complete_ary, prefix):
     return sorted([cmd for cmd in
                    complete_ary if cmd.startswith(prefix)]) + [None]
 
-# Find the next token in str string from start_pos, we return
-# the token and the next blank position after the token or
-# str.size if this is the last token. Tokens are delimited by
-# white space.
-def next_token(str, start_pos):
-    look_at = str[start_pos:-1]
-    match = re.search('\S', look_at)
-    if match:
-        pos = match.pos
-    else:
-        pos = 0
-        pass
-    next_nonblank_pos = start_pos + pos
-    next_match = re.search('\s', str[next_nonblank_pos:-1])
-    if next_match:
-        next_blank_pos = next_nonblank_pos + next_match.pos
-    else:
-        next_blank_pos = len(str)
-        pass
-    return [next_blank_pos, str[next_nonblank_pos:next_blank_pos-1]]
-
 def complete_token_with_next(complete_hash, prefix, cmd_prefix=''):
     result = []
-    for cmd_name, cmd_obj in complete_hash:
-        if cmd_name.startwith(cmd_prefix + prefix):
-            result.append([cmd_name[len(cmd_prefix):-1], cmd_obj])
+    for cmd_name in complete_hash.keys():
+        if cmd_name.startswith(cmd_prefix + prefix):
+            result.append([cmd_name[len(cmd_prefix):], complete_hash[cmd_name]])
             pass
         pass
     pass
     return sorted(result, cmp=lambda a, b: cmp(a[0].lower(), b[0].lower()))
 
+def complete_token_filtered(aliases, prefix, expanded):
+
+    """Find all starting matches in dictionary *aliases* that start
+    with *prefix*, but filter out any matches already in
+    *expanded*."""
+
+    complete_ary = aliases.keys()
+    return sorted([cmd for cmd in
+                   complete_ary if cmd.startswith(prefix)] and not (
+                       cmd in aliases and expanded not in aliases[cmd])
+                   ) + [None]
+
+def complete_token_filtered_with_next(aliases, prefix, expanded, commands):
+
+    """Find all starting matches in dictionary *aliases* that start
+    with *prefix*, but filter out any matches already in
+    *expanded*."""
+
+    complete_ary = aliases.keys()
+    expanded_ary = expanded.keys()
+    return sorted([cmd for cmd in
+                   complete_ary if cmd.startswith(prefix)] and not (
+                       cmd in aliases and
+                       0 == len(set[expanded_ary] - set[aliases[cmd]])
+                   )) + [None]
+
+def next_token(str, start_pos):
+    """Find the next token in str string from start_pos, we return
+    the token and the next blank position after the token or
+    str.size if this is the last token. Tokens are delimited by
+    white space."""
+    look_at = str[start_pos:]
+    match = re.search('\S', look_at)
+    if match:
+        pos = match.start()
+    else:
+        pos = 0
+        pass
+    next_nonblank_pos = start_pos + pos
+    next_match = re.search('\s', str[next_nonblank_pos:])
+    if next_match:
+        next_blank_pos = next_nonblank_pos + next_match.start()
+    else:
+        next_blank_pos = len(str)
+        pass
+    return [next_blank_pos, str[next_nonblank_pos:next_blank_pos]]
+
 def completer(self, str, state, last_token=''):
     next_blank_pos, token = next_token(str, 0)
     if len(token) == 0 and not 0 == len(last_token):
         return ['', None]
+    match_pairs = complete_token_with_next(self.commands(), token)
+
+    match_hash = {}
+    for pair in match_pairs:
+      match_hash[pair[0]] = pair[1]
+      pass
+
+    alias_pairs = complete_token_filtered_with_next(self.aliases(),
+                                                    token, match_hash,
+                                                    self.commands.keys())
+
     matches = complete_token(self.commands.keys() +
                              self.aliases.keys() +
                              self.macros.keys(), token)
@@ -50,4 +88,14 @@ if __name__=='__main__':
     print(complete_token(['cond', 'condition', 'continue'], 'cond'))
     print(next_token('ab cd ef', 0))
     print(next_token('ab cd ef', 2))
+    h = {'ab':1, 'aac':2, 'aa':3, 'b':4}
+    print(complete_token(h.keys(), 'a'))
+    print(complete_token_with_next(h, 'a'))
+
+    ##   0         1
+    ##   0123456789012345678
+    x = '  now is  the  time'
+    for pos in [0, 2, 5, 8, 9, 13, 19]:
+        print(next_token(x, pos))
+        pass
     pass

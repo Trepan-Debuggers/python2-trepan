6ce990d9990ef60d9a0e8f08014c356e776168ad
rocky <rocky@gnu.org>
2014-04-21 08:57:48 -0400

    Small docstring typo

--- trepan/cli.py
+++ trepan/cli.py
@@ -14,7 +14,7 @@
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-''' Thecommand-line interface to the debugger.
+'''The command-line interface to the debugger.
 '''
 import os, os.path, sys
 

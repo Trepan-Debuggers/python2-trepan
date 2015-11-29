08d64fa3647fecf6ff01cf5d44c24b9ddd519372
rocky <rocky@gnu.org>
2015-11-29 08:31:16 -0500

    Fix command completion on set style and improve help.

--- trepan/processor/cmdproc.py
+++ trepan/processor/cmdproc.py
@@ -206,6 +206,10 @@ def print_location(proc_obj):
             'reload_on_change' : proc_obj.settings('reload'),
             'output'           : proc_obj.settings('highlight')
             }
+
+        if 'style' in proc_obj.debugger.settings:
+            opts['style'] = proc_obj.settings('style')
+
         line = pyficache.getline(filename, lineno, opts)
         if not line:
             if sys.version_info[1] <= 4:

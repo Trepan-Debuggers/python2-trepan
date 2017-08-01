b688a6b06f146bb742356da2626fb94b8a317e8d
rocky <rocky@gnu.org>
2017-08-01 12:35:40 -0400

    Allow 'start_trace' options ...
    May be useful in tracing multiple threads. Fixes Issue #19
    
    Still needs documentation and testing
    
    Guard against running setup in the wrong branch.

--- setup.py
+++ setup.py
@@ -8,14 +8,17 @@ from code as much as possible. The customizable information really comes
 from file __pkginfo__.py.
 """
 
-import sys
-if not ((2, 6) <= sys.version_info[0:2] < (3, 0)):
-    mess = "Python Versions 2.6 and 2.7 are supported only in this package."
+import os, sys
+if not ((2, 4) <= sys.version_info[0:2] < (3, 0)):
+    mess = "Only Python Versions 2.4 to 2.7 are supported in this package."
     if ((3, 2) <= sys.version_info[0:2] < (3, 7)):
         mess += ("\nFor your Python, version %s, see trepan3k" % sys.version[0:3])
     elif sys.version_info[0:2] < (2, 6):
         mess += ("\nFor your Python, version %s, see pydbgr" % sys.version[0:3])
     raise Exception(mess)
+elif ((2, 4) <= sys.version_info[0:2] < (2, 6)) and not os.path.exists('gitbranch-master'):
+    raise Exception("You have the wrong code or git branch for Python 2.4, 2.5")
+
 
 # Get the package information used in setup().
 from __pkginfo__ import \
--- trepan/lib/core.py
+++ trepan/lib/core.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#   Copyright (C) 2008-2010, 2013-2015 Rocky Bernstein <rocky@gnu.org>
+#   Copyright (C) 2008-2010, 2013-2015, 2017 Rocky Bernstein <rocky@gnu.org>
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -55,7 +55,6 @@ class DebuggerCore:
         See also `start' and `stop'.
         """
 
-        import trepan.processor
         import trepan.bwprocessor as Mbwproc
 
         get_option       = lambda key: Mmisc.option_set(opts, key,
@@ -231,8 +230,10 @@ class DebuggerCore:
             # Has tracer been started?
             if not tracer.is_started() or get_option('force'):
                 # FIXME: should filter out opts not for tracer
-                # Also, if we use opts.copy we need to check for 'None'.
+
                 tracer_start_opts = default.START_OPTS.copy()
+                if opts:
+                    tracer_start_opts.update(opts.get('tracer_start', {}))
                 tracer_start_opts['trace_fn'] = self.trace_dispatch
                 tracer_start_opts['add_hook_opts'] = add_hook_opts
                 tracer.start(tracer_start_opts)

1f2e6119e11b610c9a4bc6235819e6e61963b189
rocky <rocky@gnu.org>
2017-11-15 03:46:38 -0500

    Revise min version info

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -48,10 +48,10 @@ install_requires   = ['columnize >= 0.3.9',
                       'nose==1.0.0',
                       'pyficache >= 0.3.1',
                       'pygments  == 1.4',
-                      'spark_parser >= 1.7.1, <1.8.0',
-                      'uncompyle6 >= 2.13.2',
+                      'spark_parser >= 1.7.2, <1.8.0',
+                      'uncompyle6 >= 2.13.3',
                       'tracer >= 0.3.2',
-                      'xdis >= 3.6.0, < 3.7.0',
+                      'xdis >= 3.6.1, < 3.7.0',
                       ]
 license            = 'GPL3'
 mailing_list       = 'python-debugger@googlegroups.com'

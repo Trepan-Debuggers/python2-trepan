6e6ad2d28145dc5b0600f2dbff026d7af1a23602
rocky <rocky@gnu.org>
2018-02-17 11:58:08 -0500

    Use API fn from new uncompyle6 in deparse

--- trepan/processor/command/deparse.py
+++ trepan/processor/command/deparse.py
@@ -15,31 +15,16 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os, sys
 from getopt import getopt, GetoptError
-from uncompyle6.semantics.fragments import deparse_code, deparse_code_around_offset
+from uncompyle6.semantics.fragments import (
+    deparse_code, deparse_code_around_offset)
+from uncompyle6.semantics.fragments import deparsed_find
 from trepan.lib.deparse import deparse_and_cache
-from trepan.lib.bytecode import op_at_code_loc
-from StringIO import StringIO
 from pyficache import highlight_string, getlines
 from xdis import IS_PYPY
 from xdis.magics import sysinfo2float
 
 # Our local modules
 from trepan.processor.command import base_cmd as Mbase_cmd
-
-# FIXME: put this in uncompyle6 fragments
-def deparsed_find(tup, deparsed, code):
-    nodeInfo = None
-    name, last_i = tup
-    if (name, last_i) in deparsed.offsets.keys():
-        nodeInfo =  deparsed.offsets[name, last_i]
-    else:
-        co = code.co_code
-        if op_at_code_loc(co, last_i) == 'DUP_TOP':
-            offset = deparsed.scanner.next_offset(co[last_i], last_i)
-            if (name, offset) in deparsed.offsets:
-                nodeInfo =  deparsed.offsets[name, offset]
-
-    return nodeInfo
 
 class DeparseCommand(Mbase_cmd.DebuggerCommand):
     """**deparse** [options] [ . ]
@@ -52,9 +37,8 @@ Options:
     -o | --offset [num]  show deparse of offset NUM
     -h | --help          give this help
 
-    deparse around where the program is currently stopped. If no offset is
-    given, we use the current frame offset. If `-p` is given, include parent
-    information.
+deparse around where the program is currently stopped. If no offset is given,
+we use the current frame offset. If `-p` is given, include parent information.
 
 If an '.' argument is given, deparse the entire function or main
 program you are in.
@@ -212,12 +196,12 @@ See also:
         return
     pass
 
-if __name__ == '__main__':
-    from trepan import debugger
-    d            = debugger.Debugger()
-    cp           = d.core.processor
-    command      = DeparseCommand(d.core.processor)
-    command.proc.frame = sys._getframe()
-    command.proc.setup()
-    command.run(['deparse'])
-    pass
+# if __name__ == '__main__':
+#     from trepan import debugger
+#     d            = debugger.Trepan()
+#     cp           = d.core.processor
+#     command      = DeparseCommand(d.core.processor)
+#     command.proc.frame = sys._getframe()
+#     command.proc.setup()
+#     command.run(['deparse'])
+#     pass

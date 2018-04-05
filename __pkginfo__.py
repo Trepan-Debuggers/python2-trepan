173b50eeb39c493cf524be27467d34a7d02ebc3d
rocky <rocky@gnu.org>
2018-04-05 00:17:04 -0400

    Relax xdis version for now

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -52,7 +52,7 @@ install_requires   = ['columnize >= 0.3.9',
                       'uncompyle6 >= 3.1.1',
                       'tracer >= 0.3.2',
                       'unittest2',
-                      'xdis >= 3.8.0, < 3.9.0',
+                      'xdis >= 3.7.0, < 3.9.0',
                       ]
 license            = 'GPL3'
 mailing_list       = 'python-debugger@googlegroups.com'

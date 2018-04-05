ca337b17d1e982f1b7f413de9fdb928fc88447b9
rocky <rocky@gnu.org>
2018-04-04 23:58:44 -0400

    Bump min xdis and uncompyle6 versions

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -49,10 +49,10 @@ install_requires   = ['columnize >= 0.3.9',
                       'pyficache >= 1.0.0',
                       'pygments == 2.0.0',
                       'spark_parser >= 1.8.5, <1.9.0',
-                      'uncompyle6 >= 2.16.0',
+                      'uncompyle6 >= 3.1.1',
                       'tracer >= 0.3.2',
                       'unittest2',
-                      'xdis >= 3.7.0, < 3.8.0',
+                      'xdis >= 3.8.0, < 3.9.0',
                       ]
 license            = 'GPL3'
 mailing_list       = 'python-debugger@googlegroups.com'

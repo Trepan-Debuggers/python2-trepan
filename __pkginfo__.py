bd6a5f8fef4d44bd2bb17e85b281513829951ebb
rocky <rocky@gnu.org>
2018-10-27 13:00:26 -0400

    Get ready for release 0.8.8

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -48,11 +48,11 @@ install_requires   = ['columnize >= 0.3.9',
                       'nose>=1.0.0, <= 1.3.4',
                       'pyficache >= 1.0.0',
                       'pygments == 2.0.0',
-                      'spark_parser >= 1.8.5, <1.9.0',
-                      'uncompyle6 >= 3.1.1',
+                      'spark_parser >= 1.8.7, <1.9.0',
+                      'uncompyle6 >= 3.2.4',
                       'tracer >= 0.3.2',
                       'unittest2',
-                      'xdis >= 3.8.0',
+                      'xdis >= 3.8.8',
                       ]
 license            = 'GPL3'
 mailing_list       = 'python-debugger@googlegroups.com'

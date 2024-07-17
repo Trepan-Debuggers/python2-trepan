9f4f1981813ed384048144978f4edf9a8e9abac0
rocky <rocky@gnu.org>
2024-07-17 13:15:11 -0400

    Adinisitrivia

--- admin-tools/merge-for-2.4.sh
+++ admin-tools/merge-for-2.4.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 # Setup for running Python 2.4 .. 2.7, merging python-3.0-to-3.2 into this branch
-xdis_24_owd=$(pwd)
+trepan2_24_owd=$(pwd)
 cd $(dirname ${BASH_SOURCE[0]})
 (cd .. && PYTHON_VERSION=2.4 && pyenv local $PYTHON_VERSION)
 if . ./setup-python-2.4.sh; then
     git merge master
 fi
-cd $xdis_24_owd
+cd $trepan2_24_owd

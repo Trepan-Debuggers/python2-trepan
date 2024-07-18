f7e92ee3a190888786d7dddbb18dac5f5f1d7221
rocky <rocky@gnu.org>
2024-07-18 11:39:22 -0400

    Administrivia

--- admin-tools/setup-master.sh
+++ admin-tools/setup-master.sh
@@ -22,7 +22,7 @@ mydir=$(dirname $bs)
 fulldir=$(readlink -f $mydir)
 cd $fulldir/..
 checkout_version python-spark  && \
-checkout_version python-filecache && \
+checkout_version python-filecache python-2.4-to-2.7 && \
 checkout_version python-xdis python-2.4-to-2.7 && \
 checkout_version python-uncompyle6 && \
 git checkout master && pyenv local $PYTHON_VERSION && git pull

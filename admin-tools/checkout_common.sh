3665eb5e5770de1b3be10ca5944b0daf7f41fbc9
rocky <rocky@gnu.org>
2024-12-21 02:29:21 -0500

    Administrivia

---
+++ admin-tools/checkout_common.sh
@@ -0,0 +1,20 @@
+# Common checkout routine
+export PATH=$HOME/.pyenv/bin/pyenv:$PATH
+bs=${BASH_SOURCE[0]}
+mydir=$(dirname $bs)
+
+function setup_version {
+    local repo=$1
+    version=$2
+    echo Running setup $version on $repo ...
+    (cd ./$repo && . ./admin-tools/setup-${version}.sh)
+    return $?
+}
+
+function checkout_finish {
+    branch=$1
+    cd $trepan2_owd
+    git checkout $branch && pyenv local $PYTHON_VERSION && git pull
+    rc=$?
+    return $rc
+}

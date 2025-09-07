4f46752a893cc5dcfd378f465b76fd0c0165897d
rocky <rocky@gnu.org>
2025-09-07 12:17:04 -0400

    Admnistrivia

--- admin-tools/setup-python-2.4.sh
+++ admin-tools/setup-python-2.4.sh
@@ -6,25 +6,15 @@ if [[ $0 == $bs ]] ; then
     exit 1
 fi
 
-function checkout_version {
-    local repo=$1
-    version=${2:-python-2.4-to-2.7}
-    echo Checking out $version on $repo ...
-    (cd ../$repo && git checkout $version && pyenv local $PYTHON_VERSION) && \
-	git pull
-    return $?
-}
-
-export PATH=$HOME/.pyenv/bin/pyenv:$PATH
-set_python24_owd=$(pwd)
-bs=${BASH_SOURCE[0]}
 mydir=$(dirname $bs)
+trepan2_owd=$(pwd)
 cd $mydir/..
-checkout_version python-spark python-2.4-to-2.7 && \
-checkout_version python-filecache python-2.4-to-2.7 && \
-checkout_version python-xdis python-2.4-to-2.7 && \
-checkout_version shell-term-background python-2.4-to-2.7 && \
-checkout_version python-uncompyle6 python-2.4-to-2.7 && \
-git checkout python-2.4-to-2.5 && pyenv local $PYTHON_VERSION && git pull
-remake -c clean_pyc
-cd $set_python24_owd
+(cd $mydir/../../../rocky  && \
+     setup_version python-uncompyle6 python-2.4 && \
+     setup_version python-xdis python-2.4 && \
+     setup_version python-filecache python-2.4 && \
+     setup_version shell-term-background python-2.4 && \
+     setup_version pytracer python-2.4 && \
+     setup_version pycolumnize python-2.4 \
+)
+checkout_finish python-2.4-to-2.5

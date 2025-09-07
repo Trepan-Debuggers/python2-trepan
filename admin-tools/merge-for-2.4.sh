ab7bf64e593a62e0849d8c1f0572dce6cbfcb8e2
rocky <rocky@gnu.org>
2025-09-07 12:59:46 -0400

    Administrivia

--- admin-tools/merge-for-2.4.sh
+++ admin-tools/merge-for-2.4.sh
@@ -1,9 +1,14 @@
 #/bin/bash
 # Setup for running Python 2.4 .. 2.7, merging python-3.0-to-3.2 into this branch
-trepan2_24_owd=$(pwd)
-cd $(dirname ${BASH_SOURCE[0]})
-(cd .. && PYTHON_VERSION=2.4 && pyenv local $PYTHON_VERSION)
-if . ./setup-python-2.4.sh; then
+#!/bin/bash
+PYTHON_VERSION=2.4
+
+bs=${BASH_SOURCE[0]}
+trepan_merge_24_owd=$(pwd)
+mydir=$(dirname $bs)
+cd $mydir/..
+pyenv local $PYTHON_VERSION
+if . ./admin-tools/setup-python-2.4.sh; then
     git merge master
 fi
 cd $trepan2_24_owd
